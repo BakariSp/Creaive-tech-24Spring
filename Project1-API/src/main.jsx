b00b114c8c7b621e4254cfd6b86c7fc6c0ac7962
BakariSp <cailinghang17@gmail.com>
2024-02-01 21:47:26 -0800

    update

--- Project1-API/src/main.jsx
+++ Project1-API/src/main.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Root from "./routes/root";
 import App from './App.jsx';
 import './index.css';
 
